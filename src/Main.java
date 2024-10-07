c69f111c78242d97e79e4747b16575b75e86670e
Bohdan Naumenko <131257541+setygur@users.noreply.github.com>
2024-10-07 09:07:45 +0200

    Added Adder

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 //TODO: we need to add the missing classes!!!
 
+//OK I will add Adder and s30854 will add Substractor
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(adder.add(1,2));
+        System.out.println(Adder.add(1,2));
 
         Substractor substractor = new Subtractor();
         System.out.println(substractor.substract(6,3));
+    }
+}
+
+class Adder{
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
